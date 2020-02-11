dc73ef17258a4d3132513cc8b02f72aa1625e5f2
Hiroko ABE <hihoyeahi@gmail.com>
2020-02-11 19:20:20 +0900

    front-page.phpのjsの記述を変更

--- front-page.php
+++ front-page.php
@@ -75,10 +75,14 @@
 
 
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script>
+<!-- <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script> -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.jmap.min.js"></script>
 <script>
-        $('img[usemap]').jMap();
+
+        $(window).on('load', function(){
+     $("img[usemap]").jMap();
+});
+    
 </script>
 
 </body>
