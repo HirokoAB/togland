1b9bf54540ea50636e4fe1a861ba1a01ac4c6d7c
Hiroko ABE <hihoyeahi@gmail.com>
2020-02-11 18:59:59 +0900

    レスポンシブ時のクリッカブルマップ が動作しない現象を解決するために -jsフォルダにjmap.jsを追加 -front-page.phpのjQueryバージョンを2.2.4に変更

--- front-page.php
+++ front-page.php
@@ -74,11 +74,11 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.rwdImageMaps.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.jmap.min.js"></script>
 <script>
-        $('img[usemap]').rwdImageMaps();
+        $('img[usemap]').jMap();
 </script>
 
 </body>
