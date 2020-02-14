dbc1739d226e6c871ce175f4118a1e8974c4ff90
Hiroko ABE <hihoyeahi@gmail.com>
2020-02-15 02:24:18 +0900

    クリッカブルマップ のエリアを変更 -レイヤーを削除 -キーボックスはクリッカブルのまま

--- front-page.php
+++ front-page.php
@@ -18,45 +18,58 @@
 			<div class="video_wrappar">
 			
 				<div class="map_container">
-						<div   class="map map_base">
-							<img class="base_img" style="z-index:1;"src="<?php echo get_template_directory_uri(); ?>/img/map_base_1.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示する地図のベース画像" />
+						<div  type="image/svg+xml" style="z-index: 1" class="map map_base">
+							<img class="base_img" src="<?php echo get_template_directory_uri(); ?>/img/map_base_1.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示する地図のベース画像" />
 						</div>
-						<div  type="image/svg+xml" class="map map eagle shake">
+						<div  type="image/svg+xml" style="z-index: 2" class="map map eagle shake">
 							<img class=""src="<?php echo get_template_directory_uri(); ?>/img/map_eagle.svg" alt="戸倉ネイチャーパークトップページに表示しているサイトオオワシのイラスト">	
 						</div>
-						<div  type="image/svg+xml" class="map keybox flash" id="">
-							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_keybox.svg" usemap="#tgitem" alt="" />
+						<div  type="image/svg+xml" style="z-index: 5"  class="map compass">
+							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_compass.svg" usemap="" alt="" />
 						</div>
+						<div  type="image/svg+xml" style="z-index: 6" class="map keybox flash" id="">
+							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_keybox.svg" usemap="#tgitem" alt="" />
+							<!-- <img class="guide flash" src="<?php echo get_template_directory_uri(); ?>/img/guide_2.svg" alt="戸倉ネイチャーパークトップページに表示している同じサーバー上で公開しているサイトへ遷移する宝箱の画像"> -->
+						</div>
+				</div>
 			</div>
-			<map name="tgitem">
 
-
-		<area onFocus="this.blur();" shape="rect" coords="55,51,86,72"　href="https://www.tamipack.jp/" alt="たみ子の海パックホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="" href="#" alt="釣り体験" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="110,110,147,130" href="https://o-eels.org/" alt="おきなくらイールズホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="100,81,142,108" href="####" alt="漁業体験" target="__blank" />
-		<area onFocus="this.blur();" shape="rect" coords="155,52,194,75" href="https://www.town.minamisanriku.miyagi.jp/index.cfm/8,0,39,390,html" alt="南三陸町ネイチャーセンターホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="143,29,175,49" href="https://www.facebook.com/yamagakko/" alt="波伝の森山学校ホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="100,81,142,108"ss href="http://kawatouminovisitorcenter.jp/umi/index.html" alt="海のビジターセンターホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="12,45,53,86"　href="https://www.m-kankou.jp/kamiwari-camp/" alt="神割キャンプ場ホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="88,38,119,60" href="https://www.pref.miyagi.jp/soshiki/sz-seinen/" alt="志津川自然の家ホームページへ遷移" target="_blank" />
-		<area onFocus="this.blur();" shape="rect" coords="3,104,52,148" href="<?php echo get_permalink( HOME );?> "alt="" target="_blank"/>
-			</map>
-					
-		</div>
+					<map name="tgitem">
+					  <area onFocus="this.blur();" shape="rect" coords="55,51,86,72" href="https://www.tamipack.jp/" alt="たみ子の海パックホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="50,92,85,108" href="#" alt="釣り体験" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="110,110,147,130" href="https://o-eels.org/" alt="おきなくらイールズホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="143,90,179,100" href="#" alt="漁業体験" target="__blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="155,52,194,75" href="https://www.town.minamisanriku.miyagi.jp/index.cfm/8,0,39,390,html" alt="南三陸町ネイチャーセンターホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="124,38,148,55" href="https://www.facebook.com/yamagakko/" alt="波伝の森山学校ホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="100,81,142,108" href="http://kawatouminovisitorcenter.jp/umi/index.html" alt="海のビジターセンターホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="12,45,53,86" href="https://www.m-kankou.jp/kamiwari-camp/" alt="神割キャンプ場ホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="88,38,119,60" href="https://www.pref.miyagi.jp/soshiki/sz-seinen/" alt="志津川自然の家ホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="3,104,52,148" href="<?php echo get_permalink( HOME );?> "alt="" target="_blank"/>
+					</map>
+					</div>
 			
 
 			</div>
 	</section> 
 	</main>
 
-<script src="https://code.jquery.com/jquery-3.3.0.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.rwdImageMaps.min.js"></script>
 <script>
-
-$('img[usemap]').rwdImageMaps();
-    
+        $('img[usemap]').rwdImageMaps();
 </script>
 
 </body>
